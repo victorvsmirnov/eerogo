15a838ff9fb68ed1f8345fe012474cb21beb2c07
Victor Smirnov <victorvsmirnov@gmail.com>
2024-01-16 22:45:56 +0100

    query account info

--- cmd/eeroLogin/main.go
+++ cmd/eeroLogin/main.go
@@ -20,34 +20,37 @@ func main() {
 	err = eeroclient.LoadCookie()
 	if err != nil {
 		err = eeroclient.Login()
-	if err != nil {
-		panic(err)
-	}
-	var verificatinoKey string
-	fmt.Printf("Enter verification key: ")
-	n, err := fmt.Scan(&verificatinoKey)
-	if err != nil {
-		panic(err)
-	}
-	if n == 0 {
-		panic(fmt.Errorf("null input"))
-	}
-	eeroclient.VerifyKey(verificatinoKey)
-	if err != nil {
-		panic(err)
-	}
-	err = eeroclient.SaveCookie()
-	if err != nil {
-		panic(err)
-	}
+		if err != nil {
+			panic(err)
+		}
+		var verificatinoKey string
+		fmt.Printf("Enter verification key: ")
+		n, err := fmt.Scan(&verificatinoKey)
+		if err != nil {
+			panic(err)
+		}
+		if n == 0 {
+			panic(fmt.Errorf("null input"))
+		}
+		eeroclient.VerifyKey(verificatinoKey)
+		if err != nil {
+			panic(err)
+		}
+		err = eeroclient.SaveCookie()
+		if err != nil {
+			panic(err)
+		}
 	} else {
-	err = eeroclient.LoginRefresh()
+		err = eeroclient.LoginRefresh()
 		if err != nil {
 			panic(err)
 		}
 	}
+	account, err := eeroclient.Account()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("account: %v\n", account)
+	fmt.Printf("eeroclient: %v\n", eeroclient)
 
 }
